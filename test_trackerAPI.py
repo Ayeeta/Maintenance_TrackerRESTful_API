088a6f84a7a429cfd0adbef0ad35fa197aada458
Ayeeta <elijate@gmail.com>
2018-05-30 17:12:10 +0300

    ft-rest-endpoint-#157946838

--- test_trackerAPI.py
+++ test_trackerAPI.py
@@ -1,31 +1,38 @@
 import unittest
 import json
 from run import app
-from app import User
 
 class TDApiEndpoint(unittest.TestCase):
 
-    def setUp(self):
-       # self.testEndpts = app.test_client(self)
-        
+    def setUp(self):        
         self.user = User()
         self.user.apiRe = [{'id':'maintenance',
                            'item_name':'Sony vaio',
                            'problem_desc':'anti virus out of date'}]
 
-   # def test_all_user_requests(self):
-    #    result = self.testEndpts.get('/user_requests/', data=self.userRequests)
-     #   self.assertEqual(result.status_code, 200)
-        
+          
 
-    def test_user_get_method_returns_correct_message(self):
+    def test_maintenance_req_get_method_returns_correct_message(self):
         result = self.user.get('id')
         for user in self.user.apiRe:
             if('id' == user['id']):
                 self.assertEqual((user,200), result)
         self.assertEqual(result, ('User not found', 404))
     
-    #def test_user_put_method_returns_tuple_message
+    def test_maintenance_request_post_method_returns_correct_message(self):
+        result  = self.user.post('Toshiba')
+        self.assertEqual(result, ('Created', 201)) 
+
+    def test_maintenance_request_put_method_returns_correct_message(self):
+        result = self.user.put('id')
+        for user in self.user.apiRe:
+            if('id'== user['id']):
+                self.assertEqual(result, ('Success', 200))       
+        
+
+    def test_maintenance_request_delete_method_returns_correct_message(self):
+        result = self.user.delete('id')
+        self.assertEqual(result, ('Success', 200))
 
 
 
