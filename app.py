2c052973b9216216ed90f07c0afcb75fdf1c4c8e
Ayeeta <elijate@gmail.com>
2018-05-30 09:26:20 +0300

    issue with flask_restful import module pylint error

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, jsonify, request, make_response
+
+from flask_restful import Api, Resource, reqparse
+
+
+app = Flask(__name__)
+api = Api(app)
+
+users = [
+    {
+        "name": "Nicholas",
+        "age": 42,
+        "occupation": "Network Engineer"
+    },
+    {
+        "name": "Elvin",
+        "age": 32,
+        "occupation": "Doctor"
+    },
+    {
+        "name": "Jass",
+        "age": 22,
+        "occupation": "Web Developer"
+    }
+]
+
+class User(Resource):
+    def get(self, name):
+        for user in users:
+            if(name == user["name"]):
+                return user, 200
+        return "User not found", 404
+
+    
+      
+api.add_resource(User, "/users/<string:name>")
+
+app.run(debug=True)
